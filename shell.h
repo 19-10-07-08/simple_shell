078397f7a9bc85c46ff35ae664ade45166b00798
karima jarroug <karima.jarroug.1996@gmail.com>
2023-07-26 18:49:20 -0700

    files simple

--- shell.h
+++ shell.h
@@ -112,7 +112,7 @@ typedef struct builting
 
 /* shloop.c */
 int hsh(info_t *, char **);
-int find_builtin(info_t *);
+int find_builting(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
---
+++ shell_loop.c
@@ -0,0 +1,160 @@
+#include "shell.h"
+
+/**
+ * hsh - main loop shell
+ * @info: the parameter return info struct
+ * @av: arg vector from main()
+ *
+ * Return: 0 in success, 1 in error, or error code
+ */
+int hsh(info_t *info, char **av)
+{
+	ssize_t m = 0;
+	int builtin_ret = 0;
+
+	while (m != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		m = get_input(info);
+		if (m != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
+
+/**
+ * find_builting - find builting command
+ * @info: the parameter return info struct
+ *
+ *Return: -1 if builting not found,
+ *0 if builting executed successfully,
+ *1 if builting found but not successful,
+ *2 if builting signals exit()
+ */
+int find_builting(info_t *info)
+{
+	int i, built_in_ret = -1;
+	builting_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
+
+/**
+ * find_cmd - finds command in PATH
+ * @info: the parameter return info struct
+ *
+ * Return: void
+ */
+void find_cmd(info_t *info)
+{
+	char *path = NULL;
+	int x, e;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (x = 0, e = 0; info->arg[x]; x++)
+		if (!is_delim(info->arg[x], " \t\n"))
+			e++;
+	if (!e)
+		return;
+
+	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_cmd(info);
+	}
+	else
+	{
+		if ((interactive(info) || _getenv(info, "PATH=")
+					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
+/**
+ * fork_cmd - forks an executable thread to run cmd
+ * @info: the parameter return info struct
+ *
+ * Return: void
+ */
+void fork_cmd(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}
